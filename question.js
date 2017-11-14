e76f09afdbed96b8b6f4eb6e1d4224be88594e7c
Beth Schofield <bethmschofield@gmail.com>
2017-11-14 22:12:42 +0000

    Done.

--- question.js
+++ question.js
@@ -1,1 +1,27 @@
-'use strict';
+// 'use strict';
+
+class Question {
+  constructor(question) {
+    this.content = question
+    this.save()
+  }
+
+  static All() {
+    return Question._All;
+  }
+
+  // save() {
+  //   Question._All.push(this);
+  // }
+
+  save(){
+   this.constructor._All.push(this)
+ }
+
+ static Find(id) {
+   return this.All()[id-1]
+   }
+
+}
+
+Question._All = [];
